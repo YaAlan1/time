bf7de5c824fba944b52f4d12e9fdf18abc5a95d0
YaAlan1 <chill.alanus@gmail.com>
2024-10-21 21:43:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// const timerBlock = document.querySelector('#minutes');
+// const timeBlock = document.querySelector('#seconds');
+
+
+// let minutes = 1;
+// let seconds = 1;
+
+// const int = setInterval(() => {
+//     seconds++
+//     if(seconds <= 0) {
+//         seconds = 59  
+//         minutes++
+//     }
+//     if(minutes <= 0) {
+//         minutes = 59
+//         clearInterval(int)
+//     }
+
+//     timerBlock.innerText = minutes
+//     timeBlock.innerText = seconds
+
+
+// },  1000)
+
+
